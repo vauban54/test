5331df149c63dd013d8969b3113cf31e994a4d3e
vauban54 <vauban54@gmail.com>
2021-10-06 15:27:30 +0200

    Second commit

--- src/main/java/com/example/test/HelloApplication.java
+++ src/main/java/com/example/test/HelloApplication.java
@@ -12,9 +12,11 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Ma Première app");
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
